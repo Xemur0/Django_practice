a5b6eddd1674b15dab81f4deae7f4146b9f8f4f5
Xemur0 <aleks.prfnk@gmail.com>
2022-06-06 20:59:00 +0300

    les_4

--- shop/shop/settings/prod.py
+++ shop/shop/settings/prod.py
@@ -6,3 +6,14 @@ SECRET_KEY = 'django-insecure-&7+55x^3(h%nbw&1)9&zksoe3p6wg_!$i6r4%=ij1veqy74rf$
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop',
+        'USER': 'xemur0',
+        'PASSWORD': 'qwerty123',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
